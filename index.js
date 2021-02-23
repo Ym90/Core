82a082f1dec9eddf76145f718dcaf85f4557f5a4
Steven Andrew Smith <me@igsteven.com>
2021-02-23 01:03:17 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+                                                                                   
+                   YM90 Core | Module Bot for multiple platforms                   
+                         Github: @Ym90/Core | Version 0.1                          
+                                                                                   
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// Strict Mode
+'use strict';
+
+// Load Application
+const YM90 = require('./app/app.js');
+const core = new YM90();
+
